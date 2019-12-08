02f6af101e290ac6cd4edc4952a8639256514307
Taraskiiin <taraskravets95@gmail.com>
2019-12-09 00:24:45 +0200

    kakastitlizovateyuhuitu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-$('.swords__block').click(function(){
-    $(".swords__block").removeClass(".active");
-    $(this).addClass(".active"); 
+$('.swords__block').on("click", function(){
+    $(".first__sword").toggleClass("active");
+    $(".second__sword").toggleClass("active");
+    $(".third__sword").toggleClass("active");
  });
